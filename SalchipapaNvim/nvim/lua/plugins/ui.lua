7409036cd2d613faf127ded8dd70f27a168aa630
erickm13 <mejiaerick3192@gmail.com>
2025-10-01 13:59:32 -0600

    feat: add to logo github in zellij

--- SalchipapaNvim/nvim/lua/plugins/ui.lua
+++ SalchipapaNvim/nvim/lua/plugins/ui.lua
@@ -67,11 +67,11 @@ return {
   -- Description: A Neovim plugin for viewing documentation.
   {
     "amrbashir/nvim-docs-view",
-    lazy = true,            -- Load this plugin lazily
+    lazy = true, -- Load this plugin lazily
     cmd = "DocsViewToggle", -- Command to toggle the documentation view
     opts = {
-      position = "right",   -- Position the documentation view on the right
-      width = 60,           -- Set the width of the documentation view
+      position = "right", -- Position the documentation view on the right
+      width = 60, -- Set the width of the documentation view
     },
   },
 
@@ -80,18 +80,18 @@ return {
   -- Description: A blazing fast and easy to configure Neovim statusline plugin.
   {
     "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",                                       -- Load this plugin on the 'VeryLazy' event
+    event = "VeryLazy", -- Load this plugin on the 'VeryLazy' event
     requires = { "nvim-tree/nvim-web-devicons", opt = true }, -- Optional dependency for icons
     opts = {
       options = {
-        theme = "gruvbox",    -- Set the theme for lualine
+        theme = "gruvbox", -- Set the theme for lualine
         icons_enabled = true, -- Enable icons in the statusline
       },
       sections = {
         lualine_a = {
           {
             "mode", -- Display the current mode
-            icon = "󱗞", -- Set the icon for the mode
+            icon = " ", -- Set the icon for the mode
           },
         },
       },
@@ -160,22 +160,21 @@ return {
   {
     "b0o/incline.nvim",
     event = "BufReadPre", -- Load this plugin before reading a buffer
-    priority = 1200,      -- Set the priority for loading this plugin
+    priority = 1200, -- Set the priority for loading this plugin
     config = function()
       require("incline").setup({
         window = { margin = { vertical = 0, horizontal = 1 } }, -- Set the window margin
         hide = {
-          cursorline = true,                                    -- Hide the incline window when the cursorline is active
+          cursorline = true, -- Hide the incline window when the cursorline is active
         },
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t") -- Get the filename
           if vim.bo[props.buf].modified then
-            filename = "[+] " ..
-                filename -- Indicate if the file is modified
+            filename = "[+] " .. filename -- Indicate if the file is modified
           end
 
           local icon, color = require("nvim-web-devicons").get_icon_color(filename) -- Get the icon and color for the file
-          return { { icon, guifg = color }, { " " }, { filename } }                 -- Return the rendered content
+          return { { icon, guifg = color }, { " " }, { filename } } -- Return the rendered content
         end,
       })
     end,
@@ -189,10 +188,10 @@ return {
     cmd = "ZenMode", -- Command to toggle Zen Mode
     opts = {
       plugins = {
-        gitsigns = true,                          -- Enable gitsigns integration
-        tmux = true,                              -- Enable tmux integration
+        gitsigns = true, -- Enable gitsigns integration
+        tmux = true, -- Enable tmux integration
         kitty = { enabled = false, font = "+2" }, -- Disable kitty integration and set font size
-        twilight = { enabled = true },            -- Enable twilight integration
+        twilight = { enabled = true }, -- Enable twilight integration
       },
     },
     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } }, -- Keybinding to toggle Zen Mode
