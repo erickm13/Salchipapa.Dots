a3db5032098ab5711d373f5fa36bd42e4eb0156e
erickm13 <mejiaerick3192@gmail.com>
2026-04-29 07:22:50 +0000

    feat: update homebrew formula to v2.0.5

--- homebrew-tap/Formula/salchipapa-dots.rb
+++ homebrew-tap/Formula/salchipapa-dots.rb
@@ -1,9 +1,9 @@
 class SalchipapaDots < Formula
   desc "Salchipapa dotfiles installer — Fish/Zsh, Zellij/Tmux, Neovim & CLI tools"
   homepage "https://github.com/erickm13/Salchipapa.Dots"
-  url "https://github.com/erickm13/Salchipapa.Dots/archive/refs/tags/v2.0.4.tar.gz"
-  sha256 "05b86a3a1bb6820da943be430a8be98940764119db20d29b5e5729320d3851c3"
-  version "v2.0.4"
+  url "https://github.com/erickm13/Salchipapa.Dots/archive/refs/tags/v2.0.5.tar.gz"
+  sha256 "948346bdd4e507ac0b7f69e5ce1a96baa3aad74c0cf4bc032858f0dcdc572370"
+  version "v2.0.5"
 
   def install
     bin.install "install.sh" => "salchipapa-dots"
